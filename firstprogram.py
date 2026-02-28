020e4f07bf0bb8077b6d4ee337608ba9167f76ad
Prince Kumar <163985375+princekumar4u@users.noreply.github.com>
2026-02-28 16:28:14 +0530

    Add files via upload

---
+++ firstprogram.py
@@ -0,0 +1,18 @@
+print("Hello World!")
+
+# 🎵 Song Information Program (Google Colab)
+
+print("Welcome to Song Info Program 🎶")
+
+# user input
+song = input("Enter your favourite song name: ")
+
+# checking condition
+if song.lower() == "jo wada kiya woh nibhana padega":
+    print("\nSong Details:")
+    print("Movie  : Taj Mahal (1963)")
+    print("Singers: Mohammed Rafi & Lata Mangeshkar")
+    print("Music  : Roshan")
+    print("Type   : Classic Romantic Song ❤️")
+else:
+    print("\nNice choice! Enjoy your song 🎧")
